55f96bd66aa3035935a6f6128be9657f53e01e52
THANH NHA <thanhnha.k13@gamil.com>
2023-06-16 01:05:41 +0700

    xong 2 pages login and register

--- client/src/services/AuthApi.js
+++ client/src/services/AuthApi.js
@@ -1,8 +1,46 @@
-const AuthApi = {
-    login:(email, password) => {
-        //call axios
-        //return
-    }
-}
+import axios from "axios";
+import { setCookie } from "cookies-next";
+import Router from "next/router";
 
-export default AuthApi
+const AuthApi = {
+    login: async ({ email, password }) => {
+        try {
+            let reuslt = await axios({
+                method: "POST",
+                url: process.env.NEXT_PUBLIC_SERVER_URL + "/user/login",
+                data: {
+                    email,
+                    password,
+                },
+            });
+            console.log(reuslt.data);
+            setCookie("tokenUser", reuslt.data.data.token);
+            Router.push("/");
+
+            //trả về cái token
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    register: async ({ email, password }) => {
+        try {
+            let reuslt = await axios({
+                method: "POST",
+                url: process.env.NEXT_PUBLIC_SERVER_URL + "/user/register",
+                data: {
+                    email,
+                    password,
+                },
+            });
+            console.log(reuslt);
+            // chuyển hướng về trang login
+            Router.push("/login");
+
+            //trả về cái token
+        } catch (e) {
+            console.log(e);
+        }
+    },
+};
+
+export default AuthApi;
