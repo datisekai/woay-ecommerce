a741ad1401f2d7291695c2be9e600cf4d587144f
datisekai <datly030102@gmail.com>
2023-06-17 13:57:01 +0700

    add react query

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
-import NextNProgress from 'nextjs-progressbar';
+import "../styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
-  return  <>
-  <NextNProgress color='#000' options={{ showSpinner: false }}/>
-  <Component {...pageProps} />;
-</>
+  return (
+    <>
+      <NextNProgress color="#000" options={{ showSpinner: false }} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
